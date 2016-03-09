838a9878767ad994479f8775514cbe31c8694c02
Shubhodeep Mukherjee <shubhodeep9@gmail.com>
2016-03-10 02:34:58 +0530

    Registrations added

--- server.go
+++ server.go
@@ -6,19 +6,30 @@ import (
     "github.com/alexjlockwood/gcm"
     "log"
     "encoding/json"
+    "github.com/astaxie/beego/orm"
+    _ "github.com/mattn/go-sqlite3"
 )
 
 func receiver(w http.ResponseWriter, r *http.Request) {
     var message string = ""
-    var regID string = ""
+    
     if len(r.FormValue("message"))!=0 {
         message = r.FormValue("message")
     }
-    if len(r.FormValue("regID"))!=0 {
-        regID = r.FormValue("regID")
+    o := orm.NewOrm()
+    var registrations []*Registrations
+    _, err := o.QueryTable("registrations").All(&registrations)
+    if err == orm.ErrNoRows {
+        fmt.Fprintf(w, "No registrations")
+        return
     }
+    var regIDs []string
+    for _, val := range registrations {
+        regIDs = append(regIDs,val.Regid)
+    }
+    fmt.Println(regIDs)
+    
     data := map[string]interface{}{"message": message}
-    regIDs := []string{regID}
     msg := gcm.NewMessage(data, regIDs...)
 
     // Create a Sender to send the message.
@@ -35,8 +46,36 @@ func receiver(w http.ResponseWriter, r *http.Request) {
     } // send data to client side
 }
 
+type Registrations struct{
+    Id int
+    Regid string
+} 
+
+func (a *Registrations) TableName() string {
+    return "registrations"
+}
+
+func register(w http.ResponseWriter, r *http.Request){
+    var regID string = ""
+    if len(r.FormValue("regID"))!=0 {
+        regID = r.FormValue("regID")
+    }
+    
+    o := orm.NewOrm()
+    reg := Registrations{Regid: regID}
+    _, err := o.Insert(&reg)
+    if err == nil {
+        fmt.Fprintf(w,"Success")
+    }
+    
+}
+
 func main() {
-    http.HandleFunc("/", receiver) // set router
+    orm.RegisterDriver("sqlite", orm.DRSqlite)
+    orm.RegisterDataBase("default", "sqlite3", "data.db")
+    orm.RegisterModel(new(Registrations))
+    http.HandleFunc("/send/", receiver)
+    http.HandleFunc("/register/", register)
     err := http.ListenAndServe(":9090", nil) // set listen port
     if err != nil {
         log.Fatal("ListenAndServe: ", err)
